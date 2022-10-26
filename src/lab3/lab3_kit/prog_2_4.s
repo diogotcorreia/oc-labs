7cac0a36fd29a258a476b3b1354f93ae77439278
Diogo Correia <me@diogotc.com>
2022-10-26 10:17:56 +0100

    Reduce program cycles by 1 on loop unrolling (#58)

--- src/lab3/lab3_kit/prog_2_4.s
+++ src/lab3/lab3_kit/prog_2_4.s
@@ -11,18 +11,16 @@ mult:   .word   0
         lw      $9, 0($1)      ; $9 = A[0]  ;; mult
         lw      $12, 8($1)     ; $12 = A[1]
         lw      $13, 16($1)    ; $13 = A[2]
-        
-        daddi   $1, $1, 8      ; Advance pointer to align with the loop
 
 loop:   
         dmul    $16, $12, $9   ; $16 = $12*$9 ;; $16 = A[i]*mult
-        daddi   $1, $1, 16     ; *$1 +=2 
-        lw      $12, 0($1)     ; $12 = A[i]
+        daddi   $1, $1, 16     ; *$1 +=2
+        lw      $12, 8($1)     ; $12 = A[i+1]
         dadd    $9, $9, $16    ; $9 = $9 + $16 ;; mult = mult + A[i]*mult
 
         dmul    $17, $13, $9   ; $17 = $9*$17 ;; $17 = A[i]*mult
         daddi   $5, $5, 2      ; index++
-        lw      $13, 8($1)     ; $13 = A[i+1]
+        lw      $13, 16($1)    ; $13 = A[i+2]
         dadd    $9, $9, $17    ; $9 = $9 + $17 ;; mult = mult + A[i]*mult
 
         bne     $6, $5, loop   ; Exit loop if index == N
@@ -31,7 +29,7 @@ loop:
         dadd    $9, $9, $16    ; $9 = $9 + $16 ;; mult = mult + A[i]*mult
 
         dmul    $17, $13, $9   ; $17 = $9*$17 ;; $17 = A[i]*mult
-        lw      $12, 16($1)    ; $12 = A[i+2]
+        lw      $12, 24($1)    ; $12 = A[i+3]
         dadd    $9, $9, $17    ; $9 = $9 + $17 ;; mult = mult + A[i]*mult
 
         dmul    $16, $12, $9   ; $16 = $12*$9 ;; $16 = A[i]*mult
